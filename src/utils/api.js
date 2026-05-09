ddcdd8ec4971af2f1bd8dd9607e5540b439da98c
Raman-954 <152240479+Raman-954@users.noreply.github.com>
2026-05-09 23:22:01 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-
+const API_URL = "https://sawari-backend-vxxo.onrender.com"
 // Create axios instance with credentials
 const api = axios.create({
   baseURL: API_URL,
